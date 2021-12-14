fa18b1657f61f95950f9a52a710975a60e6e7a5f
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2021-12-14 06:16:56 -0300

    created the basic structure of index

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())// express initalize bodyParser wich will convert the files to json
+
+const port = 3370
+
+app.get('/test', (req, res) => {
+    res
+    .status(200)
+    .send({ message: 'Welcome, stranger' })
+} )
+
+app.listen(port, () => console.log(`server running at ${port}`) )
